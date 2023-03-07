a29f3107ad5af84f736c8145316e9f649f29fd6f
carolmedici <carolina.obstetriz@gmail.com>
2023-03-07 15:27:26 -0300

    :sparkles: first commit
    The project is already working

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+const game = document.querySelector(".game");
+const winner = document.querySelector(".result");
+const resultP = document.querySelector(".result p");
+const fail = document.getElementById("p");
+
+const randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
+function guessingNumber(event) {
+  event.preventDefault();
+
+  const result = document.getElementById("inumber");
+
+  if (Number(result.value) == randomNumber) {
+        game.classList.add("hide");
+        winner.classList.remove('hide');
+        resultP.innerText = `You Rock! You got it in ${xAttempts} trie(s)`;
+  } else{
+    fail.innerHTML = "You missed. Try again :)";
+  }
+  result.value = ""
+  xAttempts++;
+
+}
+  
+const btnGuessNumber = document.querySelector("#guessNumber")
+const btnPlayAgain = document.querySelector("#playAgain")
+  
+btnGuessNumber.addEventListener('click', guessingNumber)
+btnPlayAgain.addEventListener('click', function(){
+    game.classList.remove("hide");
+    result.classList.add("hide");
+    xAttempts = 1
+})
+       
+
+
+/*change theme color*/
+function setColor(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+function changeTheme() {
+   if (localStorage.getItem('theme') === 'original-color'){
+       setColor('second-color');
+   } else {
+       setColor('original-color');
+   }
+}
