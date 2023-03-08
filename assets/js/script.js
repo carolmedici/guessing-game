fa09a695f42c992e17d8d2a1401dc4315afd60e1
carolmedici <carolina.obstetriz@gmail.com>
2023-03-07 21:24:48 -0300

    :ok_hand: clean code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,38 +2,42 @@ const game = document.querySelector(".game");
 const winner = document.querySelector(".result");
 const resultP = document.querySelector(".result p");
 const fail = document.getElementById("p");
-
+const btnGuessNumber = document.querySelector("#guessNumber")
+const btnPlayAgain = document.querySelector("#playAgain")
 const randomNumber = Math.round(Math.random() * 10);
+const result = document.getElementById("inumber");
 let xAttempts = 1;
+
+btnGuessNumber.addEventListener('click', guessingNumber)
+btnPlayAgain.addEventListener('click', playAgain)
 
 function guessingNumber(event) {
   event.preventDefault();
 
-  const result = document.getElementById("inumber");
-
-  if (Number(result.value) == randomNumber) {
-        game.classList.add("hide");
-        winner.classList.remove('hide');
-        resultP.innerText = `You Rock! You got it in ${xAttempts} trie(s)`;
-  } else{
+    if (result.value > 10 || result.value < 0){
+        alert('Hey! The number must be between 0-10')
+    }else if (Number(result.value) == randomNumber) {
+       
+hideToggle()
+    resultP.innerText = `You Rock! You got it in ${xAttempts} trie(s)`;
+    } else{
     fail.innerHTML = "You missed. Try again :)";
-  }
-  result.value = ""
-  xAttempts++;
-
+    }
+    result.value = ""
+    xAttempts++;
 }
   
-const btnGuessNumber = document.querySelector("#guessNumber")
-const btnPlayAgain = document.querySelector("#playAgain")
-  
-btnGuessNumber.addEventListener('click', guessingNumber)
-btnPlayAgain.addEventListener('click', function(){
-    game.classList.remove("hide");
-    result.classList.add("hide");
+function playAgain(){
+    hideToggle()
     xAttempts = 1
-})
+    randomNumber
+}
+     
+function hideToggle(){
+    game.classList.toggle('hide')
+    winner.classList.toggle('hide')
+}
        
-
 
 /*change theme color*/
 function setColor(themeName) {
